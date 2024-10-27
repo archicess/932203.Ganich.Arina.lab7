73354d0ec4f9e27c8fd343ded72e2e6ec52cd795
archicess <arina.ganich.2005@mail.ru>
2024-10-27 20:05:36 +0700

    changed

--- index.js
+++ index.js
@@ -1,47 +1,148 @@
-function addDocument() {
-    const newDocument = document.createElement('div');
-    newDocument.className = 'docContainer';
-    newDocument.innerHTML = `
-        <input type="text" class="text">
-        <input type="text" class="value">
-        <button class="Up" onclick="Up(this)">^</button>
-        <button class="Down" onclick="Down(this)">v</button>
-        <button class="Remove" onclick="removeDoc(this)">x</button>
-    `;
+const shapesContainer = document.querySelector(".shapes-container");
+const squareBtn = document.getElementById('createSquareBtn');
+const triangleBtn = document.getElementById('createTriangleBtn');
+const circleBtn = document.getElementById('createCircleBtn');
 
-    documentsList = document.getElementById("bigContainer");
-    documentsList.appendChild(newDocument);
-};
+squareBtn.addEventListener("click", function() {
+  const inputValue = document.getElementById("valuebox").value;
+  if (!checkInputValue(inputValue)) return;
+  for (let i = 0; i < parseInt(inputValue); i++) {
+    new Square();
+  }
+});
 
-function saveDocument() {
-    jsonSummary = document.querySelector(".jsonResult");
-    keyDocument = document.querySelectorAll(".text");
-    valueDoc = document.querySelectorAll(".value");
-   
-    JsonArray = [];
-    kov = "&quot";
+triangleBtn.addEventListener("click", function() {
+  const inputValue = document.getElementById("valuebox").value;
+  if (!checkInputValue(inputValue)) return;
+  for (let i = 0; i < parseInt(inputValue); i++) {
+    new Triangle();
+  }
+});
 
-    for (var i = 0; i < keyDocument.length; i++) {
-        JsonArray.push(keyDocument[i].value + kov + ":" + kov + valueDoc[i].value);
+circleBtn.addEventListener("click", function() {
+  const inputValue = document.getElementById("valuebox").value;
+  if (!checkInputValue(inputValue)) return;
+  for (let i = 0; i < parseInt(inputValue); i++) {
+    new Circle();
+  }
+});
+
+function checkInputValue(inputValue) {
+  if (inputValue <= 0 || inputValue > 10) {
+    alert("Количество фигур может быть только в диапазоне от 1 до 10");
+    return false;
+  }
+  return true;
+}
+
+class Shape {
+  constructor(shapeType) {
+    this.shapeType = shapeType;
+    this.figureSize = this.getRandomSize();
+    this.createShape();
+    this.addEventListeners();
+  }
+
+  setSize() {
+    this.shape.style.width = this.figureSize + "px";
+    this.shape.style.height = this.figureSize + "px";
+  }
+
+  createShape() {
+    this.shape = document.createElement("div");
+    this.shape.classList.add(this.shapeType);
+    this.shape.setAttribute("tabindex", "0");
+    this.setSize();
+    const windowWidth = window.innerWidth - this.figureSize;
+    this.shape.style.left = this.getRandomPosition(windowWidth);
+    const windowHeight = window.innerHeight - this.figureSize;
+    this.shape.style.top = this.getRandomPosition(windowHeight);
+    shapesContainer.appendChild(this.shape);
+  }
+
+  addEventListeners() {
+    this.shape.addEventListener("dblclick", () => {
+      this.removeShape();
+    });
+
+    this.shape.addEventListener("focus", () => {
+      this.changeColor();
+    });
+
+    this.shape.addEventListener("blur", () => {
+      this.resetColor();
+    });
+  }
+
+  removeShape() {
+    shapesContainer.removeChild(this.shape);
+  }
+
+  changeColor() {
+    this.shape.style.backgroundColor = "yellow";
+    this.setSelectedShapeFlag();
+  }
+
+  setSelectedShapeFlag() {
+    if (Shape.selectedShape && Shape.selectedShape !== this) {
+      Shape.selectedShape.resetColor();
     }
-    result ='{'+  JSON.stringify(JsonArray).slice(1, -1)+ '}';
-    jsonSummary.innerHTML = result;
-};
 
-function Up(document) {
-    previous = document.parentElement.previousElementSibling;
-    if(previous != null) {
-        document.parentElement.after(previous);
-    }
-};
+    Shape.selectedShape = this;
+  }
 
-function Down(document) {
-    next = document.parentElement.nextElementSibling;
-    if(next != null) {
-        document.parentElement.before(next);
-    }
-};
+  getRandomSize() {
+    const maxSize = 300;
+    const minSize = 30;
+    return Math.floor(Math.random() * maxSize) + minSize;
+  }
 
-function removeDoc(document) {
-    document.parentElement.remove()
+  getRandomPosition(windowSideSize) {
+    return Math.floor(Math.random() * windowSideSize) + "px";
+  }
+}
+
+class Square extends Shape {
+  constructor() {
+    super("square");
+  }
+
+  resetColor() {
+    this.shape.style.backgroundColor = 
+      this.shape.style.backgroundColor.replace("yellow", "red");
+  }
+}
+
+class Triangle extends Shape {
+  constructor() {
+    super("triangle");
+  }
+
+  setSize() {
+    console.log(this.figureSize)
+    const randomSize = this.figureSize;
+    this.shape.style.border = randomSize / 2 + "px solid transparent";
+    this.shape.style.borderBottom = randomSize/ 2 + "px  solid blue";
+  }
+
+  changeColor() {
+    this.shape.style.borderBottom = this.shape.style.borderBottom.replace("blue", "yellow");
+    this.setSelectedShapeFlag();
+  }
+
+  resetColor() {
+    this.shape.style.borderBottom = 
+      this.shape.style.borderBottom.replace("yellow", "blue");
+  }
+}
+
+class Circle extends Shape {
+  constructor() {
+    super("circle");
+  }
+
+  resetColor() {
+    this.shape.style.backgroundColor = 
+      this.shape.style.backgroundColor.replace("yellow", "green");
+  }
 }
